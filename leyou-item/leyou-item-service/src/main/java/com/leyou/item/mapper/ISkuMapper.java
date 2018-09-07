7d18614549533e3f4a043f7d3bca9a5e4803c273
xuhao <xuhaowell@qq.com>
2018-09-07 14:13:23 +0800

    添加後台商品添加功能！

---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/mapper/ISkuMapper.java
@@ -0,0 +1,14 @@
+package com.leyou.item.mapper;
+
+import com.leyou.item.pojo.Sku;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author XuHao
+ * @Title: ISkuMapper
+ * @ProjectName leyou
+ * @Description: TODO
+ * @date 2018/9/713:54
+ */
+public interface ISkuMapper extends Mapper<Sku> {
+}
